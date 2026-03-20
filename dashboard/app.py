9de9f828d4748d3e1a30c468e4deb160aeefcb62
Diego de Souza Brito <diegosbrito132@gmail.com>
2026-03-20 01:32:31 -0300

    ajuste: removendo erro

--- dashboard/app.py
+++ dashboard/app.py
@@ -15,7 +15,7 @@ df_final = carregar_dados()
 #Configuração  da pagina
 st.set_page_config(page_title="Dashboard Olist", layout="wide")
 
-# Barra Lateral (Sidebar) - Filtros [cite: 36]
+# Barra Lateral (Sidebar) 
 st.sidebar.title("Filtros do Projeto")
 data_filtro = st.sidebar.date_input("Selecione o Período")
 estado_filtro = st.sidebar.multiselect("Selecione o Estado", ["SP", "RJ", "MG", "Outros"])
@@ -35,7 +35,7 @@ col4.metric("Satisfação Média", "0.0 ⭐")
 
 st.markdown("---")
 
-#Divisão em abas (Corpo) 
+#Divisão em abas (Corpo Graficos) 
 aba1, aba2, aba3 = st.tabs(["📈 Visão Geral", "🚚 Logística", "💳 Pagamentos"])
 
 with aba1:
