55c181cf226c7108041784019fce7656da16ff00
mint kinhas <lsr.jupi@gmail.com>
2026-05-14 07:55:05 -0300

    feature: importa dados transformados e kpis no dashboard

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import streamlit as st
 import pandas as pd
+from src.extracao import baixar_bases
+from src.limpeza import limpar_arquivos
+from src.kpis import calcular_kpis
+from src.transformacao import transformar_dados
+
 
 @st.cache_data
 def carregar_dados():
-    caminho = "data/processed/df_sales_analytics.parquet"
-    try:
-        return pd.read_parquet(caminho)
-    except Exception:
-        return None
-    
-# Carga de dados
-df_final = carregar_dados()
+    baixar_bases()
+    limpar_arquivos()
+    kpis_dict = calcular_kpis()
+    df_tranformacao = transformar_dados()
 
-#Configuração  da pagina
+    return kpis_dict, df_tranformacao
+
+
+# Carga de dados
+kpis, df_final = carregar_dados()
+
+# Configuração  da pagina
 st.set_page_config(page_title="Dashboard Olist", layout="wide")
 
 # Barra Lateral  
@@ -21,21 +28,21 @@ data_filtro = st.sidebar.date_input("Selecione o Período")
 estado_filtro = st.sidebar.multiselect("Selecione o Estado", ["SP", "RJ", "MG", "Outros"])
 status_filtro = st.sidebar.selectbox("Status do pedido", ["Entregue", "Cancelado", "Todos"])
 
-#Titulo 
+# Titulo
 st.title("📊 Análise de E-commerce (Olist)")
 st.markdown("---")
 
-#Cartoes de Metricas (topo) 
+# Cartoes de Metricas (topo)
 
 col1, col2, col3, col4 = st.columns(4)
-col1.metric("Faturamento Total", "R$ 0,00")
+col1.metric("Faturamento Total", f"{kpis['faturamento_total']:.2f}")
 col2.metric("Total de pedidos", "0")
 col3.metric("Ticket Médio", "R$ 0,00")
 col4.metric("Satisfação Média", "0.0 ⭐")
 
 st.markdown("---")
 
-#Divisão em abas (Corpo Graficos) 
+# Divisão em abas (Corpo Graficos)
 aba1, aba2, aba3 = st.tabs(["📈 Visão Geral", "🚚 Logística", "💳 Pagamentos"])
 
 with aba1:
